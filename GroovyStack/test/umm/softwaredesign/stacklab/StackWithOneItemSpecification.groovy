e9cfb36c8b85e04deaea9c12a699be2550aeb09b
Nic McPhee <mcphee@morris.umn.edu>
2012-01-08 10:39:20 -0600

    Fixed some of the specification descriptions (some were incomplete and some were actually incorrect), and added a comment acknowledging the annoying code duplication in setting up the stack with one item, and suggesting some ways to fix that problem.

--- GroovyStack/test/umm/softwaredesign/stacklab/StackWithOneItemSpecification.groovy
+++ GroovyStack/test/umm/softwaredesign/stacklab/StackWithOneItemSpecification.groovy
@@ -3,8 +3,16 @@ package umm.softwaredesign.stacklab
 import spock.lang.Ignore;
 import spock.lang.Specification
 
-
 class StackWithOneItemSpecification extends Specification {
+    /*
+     * Note the persistent and annoying repetition of the code to push the
+     * one item that appears in every method in this file. This can be eliminated
+     * either with a setup() method or via a helper method. I'd lean towards a
+     * setup() method, but that forces you to change all the "then:" blocks to
+     * "expect:" blocks, and I felt that the "when/then" pairs would be easier
+     * to read in our first exposure to Spock tests so I left the repetition in
+     * for now. Groups should feel free to clean that up, though.
+     */
     def stack = new Stack<String>()
     
     def "should have size 1"() {
@@ -23,7 +31,7 @@ class StackWithOneItemSpecification extends Specification {
         !stack.isEmpty()
     }
     
-    def "top should be the pushed element"() {
+    def "top should be the pushed element & not change size"() {
         when:
         stack.push("Morris")
         
@@ -32,7 +40,7 @@ class StackWithOneItemSpecification extends Specification {
         stack.size() == 1
     }
     
-    def "pop should return pushed element & size should become 0"() {
+    def "pop should remove pushed element & size should become 0"() {
         when:
         stack.push("Morris")
         stack.pop()
